014648e27bc2ffb13e05e043c45ddc14d00e0bce
N <n@tioi.network>
2023-02-06 17:15:47 +0800

    Update page width

--- pages/details/index.tsx
+++ pages/details/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <Layout>
       <motion.div
-        className="py-16 px-5 w-full"
+        className="max-w-screen-xl mx-auto py-16 px-5 w-full"
         initial="hidden"
         whileInView="show"
         animate="show"
