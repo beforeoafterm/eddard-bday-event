c2a5e3e00683436b7d8d0e3fc96e07b1a89bab54
N <n@tioi.network>
2023-02-09 15:38:48 +0800

    Update type for Invitation page params

--- types/Invitation.types.ts
+++ types/Invitation.types.ts
@@ -1,5 +1,9 @@
+import { ParsedUrlQuery } from "querystring";
 import { Attendee } from "./Attendees.types";
 
 export interface InvitationPageProps {
   attendees: Attendee[]
+
+export interface InvitationParams extends ParsedUrlQuery {
+  invitationId: string
 }
