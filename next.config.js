0c3a4ce1a8eadde50e21e02358bc9fb83baf061c
N <n@tioi.network>
2023-02-15 16:52:11 +0800

    Add google as allowed image domains

--- next.config.js
+++ next.config.js
@@ -3,22 +3,25 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: [
+      'lh3.googleusercontent.com',
+      'www.google.com',
+    ],
   },
   async redirects() {
     return [
       {
-        source: "/github",
-        destination: "https://github.com/beforeoafterm/eddard-bday-event",
+        source: '/github',
+        destination: 'https://github.com/beforeoafterm/eddard-bday-event',
         permanent: false,
       },
-    ];
+    ]
   },
   serverRuntimeConfig: {
     API_EMAIL: process.env.API_EMAIL,
     API_PW: process.env.API_PW,
     API_URL: process.env.API_URL
   }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
