ed83e9f851ee1220cd695780ee937a19ac4cd9b7
N <ronneil@athenago.com>
2023-02-16 17:07:06 +0800

    Display invitation form

--- pages/invitation/[invitationId].tsx
+++ pages/invitation/[invitationId].tsx
@@ -7,12 +7,22 @@ import { getAttendee } from '@/lib/directus'
 import { InvitationPageProps, InvitationParams } from 'types/Invitation.types'
 
 import Rsvp from '@/components/invitation/rsvp'
+import Form from '@/components/invitation/form'
 
 export default function InvitationPage({ attendee }: InvitationPageProps) {
+
+  if (attendee.status === 'confirmed') {
+    return (
+      <Layout>
+        <Form attendee={attendee} />
+      </Layout>
+    )
+  }
+
   return (
     <Layout>
-      <Rsvp attendee={attendee}/>
-    </Layout >
+      <Rsvp attendee={attendee} />
+    </Layout>
   )
 }
 
